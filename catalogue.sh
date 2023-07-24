f0ec1681e7d76346262e5d359e5cc6e285802bd4
vgorthi <gorthivani2020@gmail.com>
2023-07-24 12:18:50 +0530

    frontend script

--- catalogue.sh
+++ catalogue.sh
@@ -1,18 +1,36 @@
-
+echo ">>>>>>>>>>>> create catalogue service<<<<<<<<<<<<"
 cp catalogue.service /etc/systemd/system/catalogue.service
+echo ">>>>>>>>>>>> create mongodb repo <<<<<<<<<<<<"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
+echo ">>>>>>>>>>>> install nodejs repos <<<<<<<<<<<<"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+echo ">>>>>>>>>>>> install node js  <<<<<<<<<<<<"
 yum install nodejs -y
+echo ">>>>>>>>>>>> create application user <<<<<<<<<<<<"
 useradd roboshop
+
+echo ">>>>>>>>>>>> create directory pf application  <<<<<<<<<<<<"
 mkdir /app
+
+echo ">>>>>>>>>>>> download application content <<<<<<<<<<<<"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+
+echo ">>>>>>>>>>>> extract application content <<<<<<<<<<<<"
 cd /app
 unzip /tmp/catalogue.zip
 cd /app
+
+echo ">>>>>>>>>>>> install nodejs dependencies <<<<<<<<<<<<"
 npm install
+echo ">>>>>>>>>>>> install mongo client <<<<<<<<<<<<"
 
 yum install mongodb-org-shell -y
+echo ">>>>>>>>>>>> load catalogue schema <<<<<<<<<<<<"
 mongo --host mongodb.gorthivani.online</app/schema/catalogue.js
+
+echo ">>>>>>>>>>>> start catalogue service<<<<<<<<<<<"
+
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl restart catalogue
