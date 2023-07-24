b8587dd6cf8eb843437e2624b72886b6a7ec05a9
vgorthi <gorthivani2020@gmail.com>
2023-07-24 14:28:24 +0530

    frontend script( we are printng inside files with highliyts)

--- catalogue.sh
+++ catalogue.sh
@@ -1,32 +1,32 @@
-echo -e "\e[36m>>>>>>>>>>>> create catalogue service<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+echo -e "\e[36m>>>>>>>>>>>> create catalogue service<<<<<<<<<<<<\e[0m"
 cp catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/robodhop.log
 
-echo -e "\e[36m>>>>>>>>>>>> create mongodb repo <<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+echo -e "\e[36m>>>>>>>>>>>> create mongodb repo <<<<<<<<<<<<\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/robodhop.log
-echo -e "\e[36m>>>>>>>>>>>> install nodejs repos <<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+echo -e "\e[36m>>>>>>>>>>>> install nodejs repos <<<<<<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/robodhop.log
 
-echo -e "\e[36m>>>>>>>>>>>> install node js  <<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+echo -e "\e[36m>>>>>>>>>>>> install node js  <<<<<<<<<<<<\e[0m"
 yum install nodejs -y &>>/tmp/robodhop.log
-echo -e "\e[36m>>>>>>>>>>>> create application user <<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+echo -e "\e[36m>>>>>>>>>>>> create application user <<<<<<<<<<<<\e[0m"
 useradd roboshop &>>/tmp/robodhop.log
 
- echo -e "\e[36m>>>>>>>>>>>> remove directory of application  <<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+ echo -e "\e[36m>>>>>>>>>>>> remove directory of application  <<<<<<<<<<<<\e[0m"
 rm -rf /app &>>/tmp/robodhop.log
 
-echo -e "\e[36m>>>>>>>>>>>> create directory of application  <<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+echo -e "\e[36m>>>>>>>>>>>> create directory of application  <<<<<<<<<<<<\e[0m"
 mkdir /app &>>/tmp/robodhop.log
 
-echo -e "\e[36m>>>>>>>>>>>> download application content <<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+echo -e "\e[36m>>>>>>>>>>>> download application content <<<<<<<<<<<<\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/robodhop.log
 
-echo -e "\e[36m>>>>>>>>>>>> extract  application content <<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+echo -e "\e[36m>>>>>>>>>>>> extract  application content <<<<<<<<<<<<\e[0m" |
 cd /app
 unzip /tmp/catalogue.zip &>>/tmp/robodhop.log
 cd /app
 
 
-echo -e "\e[36m>>>>>>>>>>>> install nodejs dependencies <<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+echo -e "\e[36m>>>>>>>>>>>> install nodejs dependencies <<<<<<<<<<<<\e[0m" |
 npm install &>>/tmp/robodhop.log
 echo -e "\e[36m>>>>>>>>>>>> install mongo client <<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
 
