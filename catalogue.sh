af88caa29f19abe3f8fa866a6eda01369b48fcd5
vgorthi <gorthivani2020@gmail.com>
2023-07-24 15:34:05 +0530

    frontend script( we are printng inside files with highliyts)

--- catalogue.sh
+++ catalogue.sh
@@ -28,11 +28,11 @@ cd /app
 
 echo -e "\e[36m>>>>>>>>>>>> install nodejs dependencies <<<<<<<<<<<<\e[0m"
 npm install &>>/tmp/roboshop.log
-echo -e "\e[36m>>>>>>>>>>>> install mongo client <<<<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>>>>> install mongo client <<<<<<<<<<<<\e[0m" |tee -a /tmp/roboshop.lob
 yum install mongodb-org-shell -y &>>/tmp/roboshop.log
-echo -e "\e[36m>>>>>>>>>>>> load catalogue schema <<<<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>>>>> load catalogue schema <<<<<<<<<<<<\e[0m" |tee -a /tmp/roboshop.lob
 mongo --host mongodb.gorthivani.online </app/schema/catalogue.js &>>/tmp/roboshop.log
-echo -e "\e[36m>>>>>>>>>>>> start catalogue service<<<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>>>>> start catalogue service<<<<<<<<<<<\e[0m" |tee -a /tmp/roboshop.lob
 
 systemctl daemon-reload &>>/tmp/roboshop.log
 systemctl enable catalogue &>>/tmp/roboshop.log
