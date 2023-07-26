52bc3618ea6136eaa8d55570814060addd02c905
vgorthi <gorthivani2020@gmail.com>
2023-07-26 16:18:58 +0530

    exit status updated

--- mysql.sh
+++ mysql.sh
@@ -1,8 +1,14 @@
+mysql_root_password=$1
+  if [ -z "${mysql_root_password}" ]; then
+    echo input password missing
+    exit 1
+    fi
 cp mysql.repo vim /etc/yum.repos.d/mysql.repo
 
 
 yum module disable mysql -y
+yum install mysql-community-server -y
+
 systemctl enable mysqld
 systemctl start mysqld
-mysql_secure_installation --set-root-pass RoboShop@1
-
+mysql_secure_installation --set-root-pass mysql_root_password
